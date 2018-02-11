651477eda0a7ae23f396c3b257c3e874bf071804
arsa92 <arsa92@outlook.com>
2018-02-11 13:36:15 +0100

    testic

---
+++ main.js
@@ -0,0 +1,21 @@
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+
+  var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+ var ourData = JSON.parse(ourRequest.responseText);
+};
+our.request.send();
+
+
+})
+
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+ var ourData = JSON.parse(ourRequest.responseText);
+};
+our.request.send();
